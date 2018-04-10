64e24f2d3a3485a28666c077717558470e54c609
Ben Gilliam <gilliabb@dukes.jmu.edu>
2018-04-10 18:34:03 -0400

    Updated layout and integrated google maps into relavant fragment

--- AutonomousVehicleApp/app/src/main/java/com/JMU/AutonomousVehicleApp/GoToFragment.java
+++ AutonomousVehicleApp/app/src/main/java/com/JMU/AutonomousVehicleApp/GoToFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -13,17 +14,25 @@ import android.view.ViewGroup;
  */
 public class GoToFragment extends Fragment {
 
+    Button click;
 
     public GoToFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        /*click = getView().findViewById(R.id.button1);
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fetchData process = new fetchData();
+                process.execute();
+            }
+        });*/
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_go_to, container, false);
     }
-
 }
--- AutonomousVehicleApp/app/src/main/java/com/JMU/AutonomousVehicleApp/MapFragment.java
+++ AutonomousVehicleApp/app/src/main/java/com/JMU/AutonomousVehicleApp/MapFragment.java
@@ -2,17 +2,28 @@ package com.JMU.AutonomousVehicleApp;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback{
 
+    MapView mapView;
+    GoogleMap map;
 
     public MapFragment() {
         // Required empty public constructor
@@ -22,8 +33,27 @@ public class MapFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View mapFragView = inflater.inflate(R.layout.fragment_map, container, false);
+
+        return mapFragView;
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getChildFragmentManager().findFragmentById(R.id.mapView);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng xlabs = new LatLng(38.431928, -78.875965);
+        MarkerOptions options = new MarkerOptions();
+        options.position(xlabs).title("X-Labs");
+        map.addMarker(options);
+        map.moveCamera(CameraUpdateFactory.newLatLng(xlabs));
+    }
 }
